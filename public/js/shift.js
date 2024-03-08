d239c0cacc7e2beb8f9eab2b967fc2b692e5f4e9
saruta <h.saruta@if-aiefu.com>
2024-03-09 05:58:57 +0900

    3/9土曜修正内容

--- public/js/shift.js
+++ public/js/shift.js
@@ -357,7 +357,7 @@ window.addEventListener('DOMContentLoaded', () => {
         for(let i = 0; i < companyKind.length; i++){
             for(let j = 0; j < companyInfo.length; j++){
                 if(companyKind[i] == companyInfo[j].getAttribute('data-company-name')){
-                    companyHeightArray[i] += companyInfo[j].closest('.getRow').clientHeight;
+                    companyHeightArray[i] += companyInfo[j].closest('.getRow').clientHeight - 0.5;
                 }
             }
         }
@@ -367,7 +367,7 @@ window.addEventListener('DOMContentLoaded', () => {
             let newElem = document.createElement('div');
             let txtElem = document.createElement('p');
             newElem.classList.add('company-view__item');
-            newElem.style.height = (companyHeightArray[i] - 8) + 'px';
+            newElem.style.height = (companyHeightArray[i] - 1) + 'px';
             txtElem.textContent = companyKind[i];
             newElem.appendChild(txtElem);
             companyView.appendChild(newElem);
